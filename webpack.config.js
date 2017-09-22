f58b25f348be1153b44f467530d36b485158cd84
root <root@basile.cns>
2017-09-22 12:41:23 +0200

    react core setup

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test : /\.js?/,loader : 'babel-loader'}
+        ]
+    }
+};
